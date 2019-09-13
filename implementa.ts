1e760723f8292747ac383c6f5074dceb560737a4
FylipePablo <fylipe5201pablo@hotmail.com>
2019-09-13 16:50:03 -0300

    Instancia de medico, enfermeiro e hospital

--- implementa.ts
+++ implementa.ts
@@ -29,5 +29,57 @@ namespace hospital{
     pacienteTres.setNome("Fylipe Pablo");
     pacienteTres.setCpf("654.684.158-78");
 
+    //instanciando o primeiro Medico e inserindo valores
+    let medicoUm = new Medico();
+    medicoUm.setCodFuncionario(87);
+    medicoUm.setNome("Meredith Grey");
+    medicoUm.setCpf("876.654.214-54");
+    medicoUm.setCrm(856387);
 
+    //instanciando o segundo Medico e inserindo valores
+    let medicoDois = new Medico();
+    medicoDois.setCodFuncionario(20);
+    medicoDois.setNome("Cristina Yang");
+    medicoDois.setCpf("687.654.153-89");
+    medicoDois.setCrm(856387);
+
+    //instanciando o terceiro Medico e inserindo valores
+    let medicoTres = new Medico();
+    medicoTres.setCodFuncionario(11);
+    medicoTres.setNome("Alex Karev");
+    medicoTres.setCpf("789.349.412-52");
+    medicoTres.setCrm(856387);
+
+    //instanciando o primeiro Enfermeiro e inserindo valores
+    let enfermeiroUm = new Enfermeiro();
+    enfermeiroUm.setCodFuncionario(56);
+    enfermeiroUm.setNome("Levi Schmitt");
+    enfermeiroUm.setCpf("635-752-026.13");
+    enfermeiroUm.setCoren(58326);
+
+    //instanciando o segundo Enfermeiro e inserindo valores
+    let enfermeiroDois = new Enfermeiro();
+    enfermeiroDois.setCodFuncionario(33);
+    enfermeiroDois.setNome("Nico Kim");
+    enfermeiroDois.setCpf("824.328.504-87");
+    enfermeiroDois.setCoren(58326);
+
+    //instanciando o terceiro Enfermeiro e inserindo valores
+    let enfermeiroTres = new Enfermeiro();
+    enfermeiroTres.setCodFuncionario(97);
+    enfermeiroTres.setNome("Owen Hunt");
+    enfermeiroTres.setCpf("636.803.453-72");
+    enfermeiroTres.setCoren(58326);
+
+    hospital.addPacientes(pacienteUm);
+    hospital.addPacientes(pacienteDois);
+    hospital.addPacientes(pacienteTres);
+
+    hospital.addEnfermeiros(enfermeiroUm);
+    hospital.addEnfermeiros(enfermeiroDois);
+    hospital.addEnfermeiros(enfermeiroTres);
+
+    hospital.addMedicos(medicoUm);
+    hospital.addMedicos(medicoDois);
+    hospital.addMedicos(medicoTres);
 }
