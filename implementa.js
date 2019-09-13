56cbecd6879b16ad6edb201d1c0b18c5430e8251
MattheusCosta <matheuscosta1921@gmail.com>
2019-09-13 16:57:47 -0300

    Script de exbição

--- implementa.js
+++ implementa.js
@@ -4,107 +4,90 @@
 ///<reference path="medico.ts" />
 ///<reference path="enfermeiro.ts" />
 ///<reference path="hospital.ts" />
-
-namespace hospital{
-
+var hospital;
+(function (hospital_1) {
     //instaciando um Hospital
-    let hospital = new Hospital();
+    var hospital = new hospital_1.Hospital();
     hospital.setNome("Grey Sloan Memorial Hospital");
-
     //instanciando o primeiro Paciente e inserindo valores
-    let pacienteUm = new Paciente();
+    var pacienteUm = new hospital_1.Paciente();
     pacienteUm.setCodPaciente(32);
     pacienteUm.setNome("Matheus Costa");
     pacienteUm.setCpf("549.654.324-56");
-
     //instanciando o segundo Paciente e inserindo valores
-    let pacienteDois = new Paciente();
+    var pacienteDois = new hospital_1.Paciente();
     pacienteDois.setCodPaciente(57);
     pacienteDois.setNome("Joâo Paulo");
     pacienteDois.setCpf("856.129.421-35");
-
     //instanciando o terceiro Paciente e inserindo valores
-    let pacienteTres = new Paciente();
+    var pacienteTres = new hospital_1.Paciente();
     pacienteTres.setCodPaciente(24);
     pacienteTres.setNome("Fylipe Pablo");
     pacienteTres.setCpf("654.684.158-78");
-
     //instanciando o primeiro Medico e inserindo valores
-    let medicoUm = new Medico();
+    var medicoUm = new hospital_1.Medico();
     medicoUm.setCodFuncionario(87);
     medicoUm.setNome("Meredith Grey");
     medicoUm.setCpf("876.654.214-54");
     medicoUm.setCrm(856387);
-
     //instanciando o segundo Medico e inserindo valores
-    let medicoDois = new Medico();
+    var medicoDois = new hospital_1.Medico();
     medicoDois.setCodFuncionario(20);
     medicoDois.setNome("Cristina Yang");
     medicoDois.setCpf("687.654.153-89");
     medicoDois.setCrm(856387);
-
     //instanciando o terceiro Medico e inserindo valores
-    let medicoTres = new Medico();
+    var medicoTres = new hospital_1.Medico();
     medicoTres.setCodFuncionario(11);
     medicoTres.setNome("Alex Karev");
     medicoTres.setCpf("789.349.412-52");
     medicoTres.setCrm(856387);
-
     //instanciando o primeiro Enfermeiro e inserindo valores
-    let enfermeiroUm = new Enfermeiro();
+    var enfermeiroUm = new hospital_1.Enfermeiro();
     enfermeiroUm.setCodFuncionario(56);
     enfermeiroUm.setNome("Levi Schmitt");
     enfermeiroUm.setCpf("635-752-026.13");
     enfermeiroUm.setCoren(58326);
-
     //instanciando o segundo Enfermeiro e inserindo valores
-    let enfermeiroDois = new Enfermeiro();
+    var enfermeiroDois = new hospital_1.Enfermeiro();
     enfermeiroDois.setCodFuncionario(33);
     enfermeiroDois.setNome("Nico Kim");
     enfermeiroDois.setCpf("824.328.504-87");
     enfermeiroDois.setCoren(58326);
-
     //instanciando o terceiro Enfermeiro e inserindo valores
-    let enfermeiroTres = new Enfermeiro();
+    var enfermeiroTres = new hospital_1.Enfermeiro();
     enfermeiroTres.setCodFuncionario(97);
     enfermeiroTres.setNome("Owen Hunt");
     enfermeiroTres.setCpf("636.803.453-72");
     enfermeiroTres.setCoren(58326);
-
     hospital.addPacientes(pacienteUm);
     hospital.addPacientes(pacienteDois);
     hospital.addPacientes(pacienteTres);
-
     hospital.addEnfermeiros(enfermeiroUm);
     hospital.addEnfermeiros(enfermeiroDois);
     hospital.addEnfermeiros(enfermeiroTres);
-
     hospital.addMedicos(medicoUm);
     hospital.addMedicos(medicoDois);
     hospital.addMedicos(medicoTres);
-
     document.getElementById("hospital").textContent = hospital.getNome();
-
     //trecho inserido para exemplificar a implentação das tabelas
-    let tabela = document.getElementById("tabelaPaciente");
-    let conteudoPaciente = " <tr> <th>Codigo</th> <th>Nome</th> <th>CPF</th> </tr>";
-    hospital.getPacientes().forEach(element => {
-      conteudoPaciente += "<tr> <td>"+element.getCodPaciente()+"</td><td>"+ element.getNome()+"</td><td>"+ element.getCpf()+"</td></tr>";
+    var tabela = document.getElementById("tabelaPaciente");
+    var conteudoPaciente = " <tr> <th>Codigo</th> <th>Nome</th> <th>CPF</th> </tr>";
+    hospital.getPacientes().forEach(function (element) {
+        conteudoPaciente += "<tr> <td>" + element.getCodPaciente() + "</td><td>" + element.getNome() + "</td><td>" + element.getCpf() + "</td></tr>";
     });
     tabela.innerHTML = conteudoPaciente;
-
-    let tabela1 = document.getElementById("tabelaEnfermeiro");
-    let conteudoEnfermeiro = " <tr> <th>Codigo</th> <th>Nome</th> <th>CPF</th> <th>Coren</th> </tr>";
-    hospital.getEnfermeiros().forEach(element =>{
-      conteudoEnfermeiro += "<tr> <td>"+element.getCodFuncionario()+"</td><td>"+ element.getNome()+"</td><td>"+ element.getCpf()+"</td><td>"+ element.getCoren()+"</td></tr>";
+    var tabela1 = document.getElementById("tabelaEnfermeiro");
+    var conteudoEnfermeiro = " <tr> <th>Codigo</th> <th>Nome</th> <th>CPF</th> <th>Coren</th> </tr>";
+    hospital.getEnfermeiros().forEach(function (element) {
+        conteudoEnfermeiro += "<tr> <td>" + element.getCodFuncionario() + "</td><td>" + element.getNome() + "</td><td>" + element.getCpf() + "</td><td>" + element.getCoren() + "</td></tr>";
     });
     tabela1.innerHTML = conteudoEnfermeiro;
-
-    let tabela2 = document.getElementById("tabelaMedico");
-    let conteudoMedico = " <tr> <th>Codigo</th> <th>Nome</th> <th>CPF</th> <th>CRM</th></tr>";
-    hospital.getMedicos().forEach(element =>{
+    var tabela2 = document.getElementById("tabelaMedico");
+    var conteudoMedico = " <tr> <th>Codigo</th> <th>Nome</th> <th>CPF</th> <th>CRM</th></tr>";
+    hospital.getMedicos().forEach(function (element) {
         //alert("Medicos: " + element.getNome());
-        conteudoMedico += "<tr> <td>"+element.getCodFuncionario()+"</td><td>"+ element.getNome()+"</td><td>"+ element.getCpf()+"</td><td>"+ element.getCrm()+"</td></tr>";
+        conteudoMedico += "<tr> <td>" + element.getCodFuncionario() + "</td><td>" + element.getNome() + "</td><td>" + element.getCpf() + "</td><td>" + element.getCrm() + "</td></tr>";
     });
     tabela2.innerHTML = conteudoMedico;
-}
+})(hospital || (hospital = {}));
